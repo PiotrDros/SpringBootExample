690b50e087f3d5416c9cb8a16034ed9982e4f750
Piotr Dros <piotrdros@gmail.com>
2017-12-02 12:14:54 +0100

    add logging datasource wrapper

---
+++ src/main/java/com/example/DatasourceProxyBeanPostProcessor.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+import java.util.concurrent.TimeUnit;
+
+import net.ttddyy.dsproxy.listener.logging.CommonsLogLevel;
+import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
+
+@Component
+public class DatasourceProxyBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof DataSource) {
+            return ProxyDataSourceBuilder.create((DataSource) bean)
+                    // logQueryBySlf4j(), logQueryByJUL(), logQueryToSysOut()
+                    .logQueryByCommons(CommonsLogLevel.INFO).countQuery()
+                    // also by sl4j, jul, system out
+                    .logSlowQueryByCommons(10, TimeUnit.MINUTES).build();
+        }
+        return bean;
+    }
+
+}
